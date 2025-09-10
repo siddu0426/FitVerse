6624c4fe1743d10897a6e85528d2faf37d3e4020
siddu0426 <msvssiddhartha@gmail.com>
2025-09-10 06:47:35 +0530

    Health app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center text-2xl">Welcome to the Fitness App</h1>
+      {/* Other components and content will go here */}
+    </div>
+  );
+}
+
+export default App;
